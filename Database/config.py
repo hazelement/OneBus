df4b0a7fcb93d5147b08cc620c207c2131e6674a
FiniteElement <harryzheng1991@gmail.com>
2016-03-14 16:43:43 -0600

    modified update_database.py
    update update_database now download zip file form transitfeed and unzip in memory.
    url is reading form city_url.config
    
    since in memory unzip is performed, delete rawdata file. now dump data direcatly into sqlite database

--- Database/config.py
+++ Database/config.py
@@ -1,10 +1,15 @@
 import pandas as pd
 import os
 
+def read_transitfeed_config(city):
+    config = pd.read_csv(os.path.dirname(os.path.realpath(__file__)) + '/city_url.config')
+    return config[config['city']==city].values[0][1]
+
 def read_api_config(item):
     config = pd.read_csv(os.path.dirname(os.path.realpath(__file__)) + '/api.config')
     return config[config['item']==item].values[0][1]
 
 
 if __name__ == "__main__":
-    print(read_api_config('fs_secret'))
+    #print(read_api_config('fs_secret'))
+    print(read_transitfeed_config('calgary_ab_canada'))
--- Database/update_database.py
+++ Database/update_database.py
@@ -3,16 +3,20 @@ import pandas as pd
 import sqlite3
 import os
 import numpy as np
+import requests
+from zipfile import ZipFile
+from io import BytesIO
+import config
 
-
-
-
-# todo add function to download gtfs zip from transitfeed.com and dump it into database
-# todo add rating from api and link to yelp pages
 
 def update_database(city_province_country):
-
+    
     print("updating database for " +city_province_country)
+    print("downloading from transitfeed.com " +city_province_country)
+    request = requests.get(config.read_transitfeed_config(city_province_country))
+    z = ZipFile(BytesIO(request.content))
+    print("download finished")
+    
     file_names=['agency', 'calendar_dates',
                 'shapes', 'stop_times', 'stops']
 
@@ -23,13 +27,13 @@ def update_database(city_province_country):
     with sqlite3.connect(db_name) as con:
         for fname in file_names:
             print("processing " + fname)
-            data = pd.read_csv(current_folder + 'RawData/' + fname +".txt")
+            data = pd.read_csv(z.open(fname + ".txt"))
             data.to_sql(fname, con, if_exists='replace')
 
         # maping service id string to integers in calendar
         fname = 'calendar'
         print("processing " + fname)
-        data = pd.read_csv(current_folder + 'RawData/' + fname +".txt")
+        data = pd.read_csv(z.open(fname + ".txt"))
 
         service_ids = np.unique(data['service_id'].values)
 
@@ -47,7 +51,8 @@ def update_database(city_province_country):
 
         fname = 'trips'
         print("processing " + fname)
-        data = pd.read_csv(current_folder + 'RawData/' + fname +".txt")
+        data = pd.read_csv(z.open(fname + ".txt"))
+        
         data.replace({"service_id": service_dict}, inplace=True)
         data[['service_id']]=data[['service_id']].astype(int)
 
@@ -61,7 +66,7 @@ def update_database(city_province_country):
 
         fname = 'routes'
         print("processing " + fname)
-        data = pd.read_csv(current_folder + 'RawData/' + fname +".txt")
+        data = pd.read_csv(z.open(fname + ".txt"))
 
         data['route_id'] = data['route_id'].str.replace('-','0')
         data[['route_id']]=data[['route_id']].astype(int)
