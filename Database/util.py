9a2f6ccdb9571a80f024a928c8d25fb5673f08b6
FiniteElement <harryzheng1991@gmail.com>
2016-03-17 10:15:54 -0600

    fix yelp api key parameter
    now yelp search take limit to 20 (maximum) and radius around user as 10000m

--- Database/util.py
+++ Database/util.py
@@ -214,7 +214,7 @@ def yelp_loc_list(lat, lng, query):
     #     'limit': '40',
     #     'radius_filter': '10000'
     # }
-    response = client.search_by_coordinates( lat, lng, accuracy=None, altitude=None,  altitude_accuracy=None, term=query )
+    response = client.search_by_coordinates( lat, lng, accuracy=None, altitude=None,  altitude_accuracy=None, term=query, limit='20', radius_filter='10000') # meter
 
 
     loc_list = []
