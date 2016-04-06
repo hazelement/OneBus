49cf8752c463f1ac34f2ecb4b58b0ac9d102965c
FiniteElement <harryzheng1991@gmail.com>
2016-04-05 20:58:08 -0600

    add loading state for search button

--- Website/app/static/script/control.js
+++ Website/app/static/script/control.js
@@ -18,6 +18,10 @@ $("#frmSearch").submit(function( event ){
                   + currentdate.getMinutes() + ":"
                   + currentdate.getSeconds()
 
+    var $btn = $('#btnSubmit').button('loading')
+
+
+
 //    $("#loading_hold").animate({ top: "0" }, 800);
     $.ajax({
         type: "POST",
@@ -25,7 +29,7 @@ $("#frmSearch").submit(function( event ){
         data: JSON.stringify({search_text: $("#txtSearch").val(),
                                 home_gps: home_gps,
                                 current_time: datetime}),
-        success: function(data){ refreshMap(data); enable_inputs();},
+        success: function(data){ refreshMap(data); $btn.button('reset');},
         contentType: "application/json",
         dataType:'json'})
 
