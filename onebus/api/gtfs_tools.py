9a9b1fd7f9c093b3bdef68ec9b03bd22315005f1
Harry Zheng <harryzheng1991@gmail.com>
2018-04-29 21:29:41 -0600

    added get nearby trips and stops

--- onebus/api/gtfs_tools.py
+++ onebus/api/gtfs_tools.py
@@ -1,5 +1,6 @@
-from models import Calender
-
+from math import cos, pi
+from datetime import datetime, timedelta
+from models import Calender, Stop, StopTime, Trip
 
 dayofweek_mapping = {0: 'monday',
                      1: 'tuesday',
@@ -10,11 +11,46 @@ dayofweek_mapping = {0: 'monday',
                      6: 'sunday'}
 
 
-def get_services(date):
+def meter_to_lat_lon(lat, lon, displacement):
+    """
+    Convert displacement in meters to changes in lat and lon
+    :param lat: float, latitude
+    :param lon: float, longitude
+    :param displacement: float, displacement in meters
+    :return: angular displacement in latitude degree, angular displacement in longitude
+    """
+    lat = lat/180 * pi
+    lon = lon/180 * pi
+    lat_displacement = 1.0/111111 * displacement
+    lon_displacement = 1.0/(111111 * cos(lat)) * displacement
+    return lat_displacement, lon_displacement
+
+
+def get_nearby_stops(lat, lon, radius=200):
+    """
+    Find bus stop around location given
+    :param lat: float, latitude
+    :param lon: float, longigude
+    :param radius: float, radius (meter) to look for
+    :return:
+    """
+
+    lat_rad, lon_rad = meter_to_lat_lon(lat, lon, radius)
+
+    kwargs = { "stop_lat__lte": lat + lat_rad,
+               "stop_lat__gte": lat - lat_rad,
+               "stop_lon__lte": lon + lon_rad,
+               "stop_lon__gte": lon - lon_rad
+    }
+
+    return Stop.objects.filter(**kwargs).all()
+
+
+def get_available_services(date):
     """
     Get service_id by date
     :param date: date object
-    :return:
+    :return: list of calender objects
     """
 
     dayofweek = dayofweek_mapping[date.weekday()]
@@ -24,4 +60,23 @@ def get_services(date):
               dayofweek: 1}
 
     return Calender.objects.filter(**kwargs).all()
+
+
+def get_nearby_trips(stops, services, current_time, time_scope=1):
+    """
+
+    :param stops: list of Stop object, nearby stops
+    :param services: list of Calender object, available services
+    :param current_time: datetime object, current time
+    :param time_scope: int, how many hours to look into in the future for trips
+    :return: list of StopTime object which contains trips, time and stop id
+    """
+
+    trips = StopTime.objects.filter(stop_id__in=stops,
+                                    arrival_time__gte=current_time.time(),
+                                    arrival_time__lte=(current_time + timedelta(hours=time_scope)).time(),
+                                    trip_id__service_id__in=services).all()
+
+    print(trips)
+
 
--- onebus/api/views.py
+++ onebus/api/views.py
@@ -4,21 +4,26 @@ from __future__ import unicode_literals
 from datetime import datetime
 from django.shortcuts import render
 
-from gtfs_tools import get_services
+from gtfs_tools import get_available_services, get_nearby_stops, get_nearby_trips
 # Create your views here.
 
 from django.http import HttpResponse
 
 
 def query(request):
+    # http://127.0.0.1:8000/api/query?lat=51.173569&lon=-114.118553&datetime=2018-04-29_18:47:05
     lat = float(request.GET['lat'])
     lon = float(request.GET['lon'])
     date_time = request.GET['datetime']
 
     datetime_object = datetime.strptime(date_time, '%Y-%m-%d_%H:%M:%S')
 
-    services = get_services(datetime_object.date())
-
+    services = get_available_services(datetime_object.date())
     print(services)
+
+    stops = get_nearby_stops(lat, lon)
+    print(stops)
+
+    get_nearby_trips(stops, services, datetime_object)
 
     return HttpResponse("Success")
