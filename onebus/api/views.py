28734340e2e45cccd1a8b9caa54bc1e01a1371ef
Harry Zheng <harryzheng1991@gmail.com>
2018-09-02 18:23:40 -0600

    set more todo list in views.py

--- onebus/api/views.py
+++ onebus/api/views.py
@@ -9,6 +9,12 @@ from utils.onebus_query import search_query
 # Create your views here.
 
 
+# TODO encapsulate POI search into classes
+# TODO encapusulate query search into classes with POIQuery class injection (maybe use factory pattern)
+# TODO test search functionality with online POI search
+# TODO setup test script for POIQuery, API testing
+# TODO POI search caching in database
+
 def query(request):
     # http://127.0.0.1:8000/api/query?lat=51.173569&lon=-114.118553&datetime=2018-04-29_18:47:05&search_word=food
     lat = float(request.GET['lat'])
