0f108b49b598967350fbe6f782d266d454edef7f
Harry Zheng <harryzheng1991@gmail.com>
2019-03-13 21:27:35 -0600

    checkpoint

--- onebus/api/models.py
+++ onebus/api/models.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
 
-# todo rename foreign key fields to non ids
 
 # todo add POI interest and save search locations to this database
 
